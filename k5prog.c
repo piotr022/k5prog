7f64d07e05507097a8f83c025d3f2824c36da8ce
Wouter van Gulik <pa3wle@gmail.com>
2023-10-02 21:50:42 +0200

    Add _DEFAULT_SOURCE to silence GCC warnings about usleep and cfmakeraw

--- k5prog.c
+++ k5prog.c
@@ -35,6 +35,10 @@
  *
  */
 
+
+/* Add _DEFAULT_SOURCE to silence warnings on usleep and cfmakeraw. */
+#define _DEFAULT_SOURCE
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/select.h>
